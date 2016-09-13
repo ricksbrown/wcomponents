3a26c733a8af3e9d9cc287363d3de2e9e37ff25a
Rick <ricksbrown73@gmail.com>
2016-09-13 14:26:52 +1000

    Totally unfinished experiments

--- wcomponents-theme/src/main/js/renderer/numberField.js
+++ wcomponents-theme/src/main/js/renderer/numberField.js
@@ -1,28 +1,25 @@
 define(["react", "wc/ui/numberField"], function(React, numberField) {
-	var NumberField = React.createClass({
-		propTypes: {
-			readonly: React.PropTypes.bool,
-			id: React.PropTypes.string.isRequired,
-			min: React.PropTypes.number,
-			max: React.PropTypes.number,
-			step: React.PropTypes.number,
-			defaultValue: React.PropTypes.number
-		},
-		render: function() {
-			return React.createElement("input", {
-				type: "number",
-				autoComplete: "off",
-				id: this.props.id,
-				name: this.props.id,
-				min: this.props.min,
-				max: this.props.max,
-				step: this.props.step,
-				defaultValue: this.props.defaultValue
-			});
-		},
-		componentDidMount: function($event) {
-			console.log("cdm", $event);
-		}
-	});
+	function NumberField(props) {
+		return React.createElement("input", {
+			type: "number",
+			autoComplete: "off",
+			id: props.id,
+			name: props.id,
+			min: props.min,
+			max: props.max,
+			step: props.step,
+			defaultValue: props.defaultValue
+		});
+	}
+
+	NumberField.propTypes = {
+		readonly: React.PropTypes.bool,
+		id: React.PropTypes.string.isRequired,
+		min: React.PropTypes.number,
+		max: React.PropTypes.number,
+		step: React.PropTypes.number,
+		defaultValue: React.PropTypes.number
+	};
+
 	return NumberField;
 });
